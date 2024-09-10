33e17f3d5dbeca788bc9434c7c7b940faeddb54b
corpi <lj845854@gmail.com>
2024-09-10 10:33:37 +0900

    config: 스프링 부트 버전 2 설정

---
+++ src/main/java/green/ecomap/tmdb_clone_coding/TmdbCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package green.ecomap.tmdb_clone_coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdbCloneCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmdbCloneCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/green/ecomap/tmdb_clone_coding/TmdbCloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package green.ecomap.tmdb_clone_coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdbCloneCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
