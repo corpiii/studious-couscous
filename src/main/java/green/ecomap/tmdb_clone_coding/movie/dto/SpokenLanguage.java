186110b33f1e5319a9fa97808738538f71bd57d7
corpi <lj845854@gmail.com>
2024-09-13 11:10:41 +0900

    feat: MovieDTO의 일부 Language 데이터를 위한 SpokenLanguage 생성
    응답값 중 spokenLanguage에 필요한 데이터는 Language의 전체 정보가 아닌 iso_639_1과 englishName, name만 필요합니다.
    필요한 데이터만 모은 DTO를 생성합니다.

---
+++ src/main/java/green/ecomap/tmdb_clone_coding/movie/dto/SpokenLanguage.java
@@ -0,0 +1,21 @@
+package green.ecomap.tmdb_clone_coding.movie.dto;
+
+import green.ecomap.tmdb_clone_coding.language.domain.Language;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SpokenLanguage {
+    private String englishName;
+    private String iso_639_1;
+    private String name;
+
+    public static SpokenLanguage from(Language language) {
+        return new SpokenLanguage(
+            language.getEnglishName(),
+            language.getIso_639_1(),
+            language.getName()
+        );
+    }
+}
