393a72e0bcd98e85db5c1ae0f902a08722ffadea
corpi <lj845854@gmail.com>
2024-09-14 22:11:17 +0900

    feat: 추천 영화 조회 페이지네이션 SQL 작성
    - 영화의 장르가 많이 겹치는 순으로 내림차순 정렬
    - 겹치는 장르의 수가 같다면 인기도를 내림차순으로 정렬

--- src/main/java/green/ecomap/tmdb_clone_coding/movie/repository/MovieRepository.java
+++ src/main/java/green/ecomap/tmdb_clone_coding/movie/repository/MovieRepository.java
@@ -1,6 +1,19 @@
 package green.ecomap.tmdb_clone_coding.movie.repository;
 
 import green.ecomap.tmdb_clone_coding.movie.domain.MovieInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface MovieRepository extends JpaRepository<MovieInfo, Long> { }
+public interface MovieRepository extends JpaRepository<MovieInfo, Long> {
+    @Query("select m, count(g) as genreCount " +
+            "from MovieInfo m " +
+            "join m.genres g " +
+            "where m.id != :id and g in " +
+            "(select g2 from MovieInfo m2 join m2.genres g2 where m2.id = :id) " +
+            "group by m " +
+            "order by genreCount desc, popularity desc")
+    Page<MovieInfo> findAllByIdOrderByGenreDescAndPopularityDesc(@Param("id") Long movieId, Pageable pageable);
+}
